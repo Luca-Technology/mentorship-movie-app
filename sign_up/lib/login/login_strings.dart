835513b8e7db7a66d00101775033cfc739426000
Fernando Augusto <fernando.augustols@outlook.com>
2023-02-07 16:03:28 -0300

    initial commit

---
+++ sign_up/lib/login/login_strings.dart
@@ -0,0 +1,11 @@
+class LoginStrings {
+  static const assetImageText = 'assets/images/fundo_pipoca.jpg';
+  static const titleText = 'Filmes Flutter';
+  static const subtitleText = 'Sign Up';
+  static const errorEmailText = 'Can\'t be empty';
+  static const emailHintText = 'name@example.com';
+  static const emailLabelText = 'E-mail';
+  static const errorTextPassword = 'Must have at least 8 characters';
+  static const passwordLabelText = 'Password';
+  static const buttonText = 'Sign in';
+}
